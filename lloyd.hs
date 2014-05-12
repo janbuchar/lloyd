7e7aa458babab683c4d9a1033b58fa1651248213
Teyras <teyras@gmail.com>
2014-05-12 18:19:37 +0200

    solve returns Nothing when there's no solution

--- lloyd.hs
+++ lloyd.hs
@@ -2,15 +2,11 @@ import Data.List (elemIndex)
 
 -- Takes the dimensions of the board and a list describing the board.
 -- Returns a list of fields to swap with the zero field
-solve :: Int -> Int -> [Int] -> [Int]
+solve :: Int -> Int -> [Int] -> Maybe [Int]
 solve x y board 
-        | length board /= x * y = noSolution
-        | not (elem 0 board) = noSolution
-        | otherwise = [0]
-
--- A special value that is returned iff the puzzle has no solution
-noSolution :: [Int]
-noSolution = [-1]
+        | length board /= x * y = Nothing
+        | not (elem 0 board) = Nothing
+        | otherwise = Just [0] -- Placeholder
 
 -- The goal state for given dimensions
 goalState :: Int -> Int -> [Int]
